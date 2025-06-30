eb78f48ce68b1242b7b94c3cce2c25695a7187f1
AZubochenko <azubochenko@ibs.ru>
2025-06-30 21:57:10 +0300

    upd

--- lesson_12/practice.py
+++ lesson_12/practice.py
@@ -34,6 +34,14 @@
 # Заголовок
 # =========
 
+def format_header(text, level=1, char="="):
+    line = char * len(text) #'========='
+    header_lines = [line] * level #['=========', '=========']
+    final_line = '\n'.join(header_lines) #"=========\n========="
+    return final_line + f"\n{text}\n" + final_line
+
+print(format_header("Заголовок", 3, "_"))
+
 # Задача с пометкой для самостоятельного изучения
 # Напишите функцию create_html_tag(tag, content, style=None, id_name=None), 
 # которая создает строку с HTML-тегом. Атрибуты style и id_name необязательны.
