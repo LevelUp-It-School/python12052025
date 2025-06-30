c75f1bfd9597d88f1e217a4c3a53d9c01ef7e906
AZubochenko <azubochenko@ibs.ru>
2025-06-30 20:59:59 +0300

    practice

--- lesson_11/arguments.py
+++ lesson_11/arguments.py
@@ -60,8 +60,59 @@
 # print(final_price(100, 200, 300, 400, 500, discount=5, price_high=200))
 # print(final_price(100, 200, 300, 400, 500, discount=5, price_low=200, price_high=350))
 
-def only_pos(x):
-    return x > 0
+# def only_pos(x):
+#     return x > 0
 
-result = filter(only_pos, [-1, 5, 6, -10, 0])
-print(", ".join(str(x) for x in result))
+# result = filter(only_pos, [-1, 5, 6, -10, 0])
+# print(", ".join(str(x) for x in result))
+
+# result = filter(str.isalpha, '12345ABCDefgh$()$@@')
+# print("".join(result))
+
+# def square(x):
+#     return x ** 2
+
+# map(<функция обработчик>, <итерируемый объект>)
+# result = map(square, range(5)) # [0, 1, 2, 3, 4]
+# print(", ".join(str(x) for x in result))
+
+# result = map(str.lower, ["abCDF", 'EFGh', 'IJkl'])
+# print('\n'.join(result))
+
+# numbers = list(map(int, input().split()))
+# print(numbers)
+
+# "1 2 43 6 3".split() -> ["1","2", "43", "6", "3"]
+
+# lambda <аргумент>: <тело функции>
+
+# lambda x: x > 0
+
+# def only_pos(x):
+#     return x > 0
+
+# result = filter(lambda x: x > 0, [-1, 5, 6, -10, 0])
+# print(", ".join(str(x) for x in result))
+
+# def square(x):
+#     return x ** 2
+
+# result = map(lambda x: x ** 2, range(5)) # [0, 1, 2, 3, 4]
+# print(", ".join(str(x) for x in result))
+
+# lines = ['abcd', 'АБ', 'abc', 'abcdef']
+# print(sorted(lines, key=lambda line: len(line)))
+
+# lines = ['abcd',  'abc', 'abcdef', 'ba', 'ab']
+# print(sorted(lines, key=lambda line: (-len(line), line)))
+
+# lines = ['abcd',  'abc', 'ba', 'ab', 'acde']
+# print(min(lines, key=lambda line: (-len(line), line)))
+
+# result = (x for x in [-1, 5, 6, -10, 0] if x > 0)
+# print(", ".join(str(x) for x in result))
+
+# from functools import reduce
+
+# numbers = range(1, 6)
+# print(reduce(lambda x, y: x + y, numbers))
