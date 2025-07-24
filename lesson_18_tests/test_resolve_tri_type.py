2fc6311ab3cc5912c4248468c79defd7c951148c
AZubochenko <azubochenko@ibs.ru>
2025-07-24 19:33:58 +0300

    lesson)18 upd

---
+++ lesson_18_tests/test_resolve_tri_type.py
@@ -0,0 +1,35 @@
+from main import resolve_tri_type
+import pytest
+
+def test_resolve_tri_type():
+    a, b, c = 3, 4, 7
+    result = resolve_tri_type(a, b, c)
+    assert result == "Разносторонний"
+
+def test_resolve_tri_type_degree():
+    a, b, c = 3, 4, 5
+    result = resolve_tri_type(a, b, c)
+    assert result == "Прямоугольный"
+    a, b, c = 4, 3, 5
+    result = resolve_tri_type(a, b, c)
+    assert result == "Прямоугольный"
+    a, b, c = 3, 5, 4
+    result = resolve_tri_type(a, b, c)
+    assert result == "Прямоугольный"
+
+
+def test_resolve_tri_type_isosceles():
+    a, b, c = 6, 5, 6
+    result = resolve_tri_type(a, b, c)
+    assert result == "Равнобедренный"
+    a, b, c = 5, 6, 6
+    result = resolve_tri_type(a, b, c)
+    assert result == "Равнобедренный"
+    a, b, c = 6, 6, 5
+    result = resolve_tri_type(a, b, c)
+    assert result == "Равнобедренный"
+
+def test_resolve_tri_type_incorrect_input():
+    a, b, c = -1, 0, 1
+    with pytest.raises(ValueError):
+        resolve_tri_type(a, b, c)
